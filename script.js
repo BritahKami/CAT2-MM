01d54c1741dc81acf3ea44d1e71e619260bb5a3f
BritahKami <mukamibridget7@gmail.com>
2026-06-26 15:22:24 +0300

    Initial multimedia assignment

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const video = document.getElementById("wildlifeVideo");
+const button = document.getElementById("toggleBtn");
+
+button.addEventListener("click", () => {
+
+```
+if (video.paused) {
+
+    video.style.display = "block";
+    video.play();
+
+} else {
+
+    video.style.display = "none";
+
+}
+```
+
+});
